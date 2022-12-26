5ee65428e5700d769e44cb00fe37222937a739ff
Hans Figo <hambasahaya0303@gmail.com>
2022-12-26 23:24:30 +0700

    new file:   .gitignore 	new file:   miningGame.cpp 	new file:   saveGames.txt 	new file:   tempCodeRunnerFile.cpp 	new file:   test.cpp

---
+++ test.cpp
@@ -0,0 +1,23 @@
+// Tugas PTI 14 Desember
+// Claudio Hans Figo
+// 22.12.2321
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <ctype.h>
+
+using namespace std;
+
+int hp, copper, silver, gold, coin, med, earnings[20], scoreIndex = 0;
+int isGameOver, user;
+string userName[20], dif;
+
+int main()
+{
+   cout << 1/2 ;
+    return 0;
+}
